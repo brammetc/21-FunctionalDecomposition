fc890e8c3c958153bc8fa98f7174b8e3f1f04dfb
brammetc <brammetc@rose-hulman.edu>
2019-01-25 12:59:33 -0500

    DONE

--- src/m1_hangman.py
+++ src/m1_hangman.py
@@ -8,10 +8,14 @@ Authors: Tanner Brammeier and Haley Braker.
 
 ####### Do NOT attempt this assignment before class! #######
 import random
+
+
 def main():
     print('started')
-    word,integer2 = stage2()
-    stage3(word)
+    word, integer2 = stage2()
+    char = stage3(word, integer2)
+    stage4(word, integer2)
+
 
 def stage1():
     with open('words.txt') as f:
@@ -22,6 +26,7 @@ def stage1():
         item = words[r]
         print(item)
     return item
+
 
 def stage2():
     print("I will choose a random secret word from a dictionary. You set the MINIMUM length of that world.")
@@ -42,14 +47,27 @@ def stage2():
             break
     return word, integer2
 
-def stage3(word):
+
+def stage3(word, integer2):
     string = input("What letter do you want to try?")
     char = str(string)
     for k in range(0, len(word)):
         if char == word[k]:
             print("yay")
-            return
+            print('You still have', integer2, 'tries left!')
+            return char, integer2
     print('No!!!!')
-    return
+    integer2 = integer2 - 1
+    print('You have', integer2, 'tries left!')
+    return integer2
 
+
+def stage4(word, integer2):
+    while True:
+        if integer2 > 0:
+            # for k in range(integer2-1):
+            stage3(word, integer2)
+            return word, integer2
+        else:
+            print("You are out of tries!")
 main()
